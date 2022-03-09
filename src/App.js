5f7fee497547062c07ccb4883f9893c9e6d00888
omerenma <godwin2341@gmail.com>
2022-03-09 11:07:06 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import {Buttons} from '../src/components/index'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+	return (
+		<div className="App">
+			<header className="App-header">
+        <Buttons cancel>Delete</Buttons>
       </header>
-    </div>
-  );
+		</div>
+	);
 }
 
 export default App;
